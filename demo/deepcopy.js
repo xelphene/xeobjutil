aa325a1a07ae4ed75cb68db96687fb6b875476a5
Steve Benson <steve@xelphene.net>
2023-06-16 19:36:12 +0000

    Fix bug and expand deepcopy demo

--- demo/deepcopy.js
+++ demo/deepcopy.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const ours = require('../index').ours;
-const deepcopy = require('../index').deepcopy;
+const deepCopy = require('../index').deepCopy;
 
 var o = {
     a: 1,
@@ -17,9 +17,16 @@ var o = {
     },
 };
 
-var c = deepcopy(o);
+var c = deepCopy(o);
 
-o.c.cB = 'orig change';
-o.c.cO.x = 'orig change';
+o.c.cB = 'changed in orig';
+o.c.cO.x = 'changed in orig';
 
-console.log(c);
+console.log(o.c.cB); // 'changed in orig'
+console.log(c.c.cB); // 'orig value'
+
+// both 'changed in orig' because the object at o.c.cO is not an
+// xeobjutil-marked object so it wasn't deep-copied.  o.c.cO and c.c.cO are
+// references to the same JS object.
+console.log(o.c.cO.x);
+console.log(c.c.cO.x);
